704e9019454153822015b99ef2152f64e49cf596
piyushkhurana2019 <piyushkhurana159@gmail.com>
2023-09-16 20:29:46 +0530

    Bundled All JS files and modified alerts

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,7 @@
-const login = async (email, password) => {
+import axios from 'axios';
+import { showAlert } from './alert';
+
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -10,20 +13,13 @@ const login = async (email, password) => {
     });
 
     if(res.data.status === 'success'){
-      alert("Logged In Successfully")
+      showAlert('success','Logged In Successfully')
       window.setTimeout(()=>{
         location.assign('/');
       }, 1000);
     }
 
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error',err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,7 +1,50 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicGl5dXNoa2h1cmFuYTIwMTkiLCJhIjoiY2xtajJqNTFsMDBmYjJpbzFvYTc3dGdjeSJ9.Q1I5rjTzgDJec3f7IrI7Ew';
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v12', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9, // starting zoom
-});
+/* eslint-disable */
+export const displayMap = (locations) => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoicGl5dXNoa2h1cmFuYTIwMTkiLCJhIjoiY2xtajJqNTFsMDBmYjJpbzFvYTc3dGdjeSJ9.Q1I5rjTzgDJec3f7IrI7Ew';
+
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    scrollZoom: true,
+    // center: [-118.113491, 34.111745],
+    zoom: 10,
+    interactive: true,
+  });
+
+  const bounds = new mapboxgl.LngLatBounds();
+
+  locations.forEach((loc) => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom',
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
+
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
+
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
+
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
+  });
+};
